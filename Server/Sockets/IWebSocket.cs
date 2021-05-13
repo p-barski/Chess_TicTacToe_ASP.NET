6283a8696b14e91046aac95d9cd8d58a6d95f193
p-barski <patryk.kubarski@protonmail.com>
2021-05-13 13:36:22 +0200

    Removed unnecessary methods and properties from IWebSocket

--- Server/Sockets/IWebSocket.cs
+++ Server/Sockets/IWebSocket.cs
@@ -7,14 +7,8 @@ namespace Server.Sockets
 {
 	public interface IWebSocket : IDisposable
 	{
-		string CloseStatusDescription { get; }
-		WebSocketCloseStatus? CloseStatus { get; }
 		WebSocketState State { get; }
-		string SubProtocol { get; }
-		void Abort();
 		Task CloseAsync(WebSocketCloseStatus closeStatus,
-			string statusDescription, CancellationToken cancellationToken);
-		Task CloseOutputAsync(WebSocketCloseStatus closeStatus,
 			string statusDescription, CancellationToken cancellationToken);
 		Task<WebSocketReceiveResult> ReceiveAsync(
 			ArraySegment<byte> buffer, CancellationToken cancellationToken);
--- Server/Sockets/WebSocketWrapper.cs
+++ Server/Sockets/WebSocketWrapper.cs
@@ -8,17 +8,10 @@ namespace Server.Sockets
 	public class WebSocketWrapper : IWebSocket
 	{
 		private readonly WebSocket socket;
-		public string CloseStatusDescription { get => socket.CloseStatusDescription; }
-		public WebSocketCloseStatus? CloseStatus { get => socket.CloseStatus; }
 		public WebSocketState State { get => socket.State; }
-		public string SubProtocol { get => socket.SubProtocol; }
 		public WebSocketWrapper(WebSocket socket)
 		{
 			this.socket = socket;
-		}
-		public void Abort()
-		{
-			socket.Abort();
 		}
 		public void Dispose()
 		{
@@ -28,11 +21,6 @@ namespace Server.Sockets
 			string statusDescription, CancellationToken cancellationToken)
 		{
 			await socket.CloseAsync(closeStatus, statusDescription, cancellationToken);
-		}
-		public async Task CloseOutputAsync(WebSocketCloseStatus closeStatus,
-			string statusDescription, CancellationToken cancellationToken)
-		{
-			await socket.CloseOutputAsync(closeStatus, statusDescription, cancellationToken);
 		}
 		public async Task<WebSocketReceiveResult> ReceiveAsync(
 			ArraySegment<byte> buffer, CancellationToken cancellationToken)
